36ca85145b054f15634335d60c77dbdeba976f2b
divyasreegedela <divyasreegedela99@gmail.com>
2026-07-02 10:00:00 +0530

    Day 2: Control Flow & Math Programs (Factorial, Palindrome, ReverseNumber)

---
+++ JavaPrograms/src/com/day2/Palindrome.java
@@ -0,0 +1,26 @@
+package com.day2;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int original = num;
+        int reverse = 0;
+
+        while (num != 0) {
+            reverse = reverse * 10 + num % 10;
+            num /= 10;
+        }
+
+        if (original == reverse)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+
+        sc.close();
+    }
+}
