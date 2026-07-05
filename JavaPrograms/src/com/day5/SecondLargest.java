87904effe94e081f2d9794125ea9cbd98cf2b4ba
divyasreegedela <divyasreegedela99@gmail.com>
2026-07-05 10:00:00 +0530

    Day 5: Array & String Manipulation (CharacterFrequency, SecondLargest)

---
+++ JavaPrograms/src/com/day5/SecondLargest.java
@@ -0,0 +1,42 @@
+package com.day5;
+import java.util.Scanner;
+
+public class SecondLargest {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("There is no second largest element.");
+        } else {
+            System.out.println("Second Largest Element = " + secondLargest);
+        }
+
+        sc.close();
+    }
+}
